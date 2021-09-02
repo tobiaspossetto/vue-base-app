e67a8817cc91130c12283203a7dfd78bfec6cefc
Pablo Abratte <pabratte@gmail.com>
2021-09-02 15:31:46 -0300

    feat: main component

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Vue base app
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data: function(){
+    return {
+      // put your variables here...
+    }
+  },
+  methods: {
+    // put your methods here...
+  }
+}
+</script>
+
+<style>
+
+</style>
